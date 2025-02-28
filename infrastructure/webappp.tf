babb8c2480f800a267f4a5d3443f0857791be992
Yvi-compt <yvesplacide@gmail.com>
2025-02-28 12:39:24 -0500

    Update webappp.tf

--- infrastructure/webappp.tf
+++ infrastructure/webappp.tf
@@ -1,15 +1,15 @@
 resource "azurerm_service_plan" "appserviceplan" {
   name                = "socialmedia-plan"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.yvisocialrg.name
+  location            = azurerm_resource_group.yvisocialrg.location
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "backend" {
   name                = "socialmedia-backend"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.yvisocialrg.name
+  location            = azurerm_resource_group.yvisocialrg.location
   service_plan_id     = azurerm_service_plan.appserviceplan.id
 
   site_config {
