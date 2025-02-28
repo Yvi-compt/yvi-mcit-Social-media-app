bd3073f3f67ec7350aabf28c9900086efbb56a70
Yvi-compt <yvesplacide@gmail.com>
2025-02-28 12:50:08 -0500

    Update database.tf

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -1,15 +1,15 @@
 resource "azurerm_cosmosdb_account" "mongodb" {
   name                = "socialmedia-mongodb"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.yvisocialrg.location
+  resource_group_name = azurerm_resource_group.yvisocialrg.name
   offer_type          = "Standard"
   kind                = "MongoDB"
 }
 
 resource "azurerm_redis_cache" "redis" {
   name                = "socialmedia-redis"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.yvisocialrg.location
+  resource_group_name = azurerm_resource_group.yvisocialrg.name
   capacity            = 1
   family              = "C"
   sku_name            = "Basic"
